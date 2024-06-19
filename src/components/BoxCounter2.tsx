41b9628167da66cb8bbc59a363a3b083e1d50110
Maria Kazakova <mariakazakova92@gmail.com>
2024-06-19 14:51:14 +0300

    added logic to the BoxCounter2

--- src/components/BoxCounter2.tsx
+++ src/components/BoxCounter2.tsx
@@ -1,21 +1,29 @@
-import {FunctionComponent} from "react";
+import {FunctionComponent, useState} from "react";
 import {BoxCounter} from "./BoxCounter";
 import {ElWrapper} from "./ElWrapper";
 import {Button} from "./Button";
 
 export const BoxCounter2: FunctionComponent = () => {
-    const onClickHandler = () => {
-
+    const initValue = 0;
+    const maxValue = 5;
+    const [state, setState] = useState(initValue)
+    const increment = () => {
+        if(state < maxValue){
+            setState(state + 1)
+        }
+    }
+    const reset = () => {
+        setState(initValue)
     }
 
     return (
         <BoxCounter>
             <ElWrapper>
-                <div>count</div>
+                <div className={state === maxValue ? 'finish' : ''}>{state}</div>
             </ElWrapper>
             <ElWrapper flexDirection={'row'}>
-                <Button name={'inc'} handleClick={onClickHandler} color={'#03a9f482'}/>
-                <Button name={'reset'} handleClick={onClickHandler} color={'#03a9f482'}/>
+                <Button name={'inc'} handleClick={increment} color={'#03a9f482'} isDisabled={state === maxValue}/>
+                <Button name={'reset'} handleClick={reset} color={'#03a9f482'}/>
             </ElWrapper>
 
         </BoxCounter>
